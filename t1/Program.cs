681d56d0ceca83313270e15ae1ecef273e7138cb
glenn swonk <gswonk@hotmail.com>
2018-03-10 20:16:38 -0800

    ok

--- t1/Program.cs
+++ t1/Program.cs
@@ -8,12 +8,21 @@ namespace t1
 {
     class Program
     {
+        // https://blog.velingeorgiev.com/vsts-personal-access-tokens-with-git-visual-studio-2017
+
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, world");
             Console.WriteLine("Hello, world");
             Console.WriteLine("Hello, world");
             Console.WriteLine("Hello, world");
+
+            Console.WriteLine("The rains in Spain fall mainly on the plains...");
+            Console.WriteLine("The rains in Spain fall mainly on the plains...");
+            Console.WriteLine("The rains in Spain fall mainly on the plains...");
+            Console.WriteLine("The rains in Spain fall mainly on the plains...");
+            
         }
     }
 }
